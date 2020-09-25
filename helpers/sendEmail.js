97a560147fc6f1b3781d3f0ee29d8f104df07c6e
ahsanbilalas <engr.ahsan.bilal@gmail.com>
2020-09-25 13:57:37 +0500

    adding smtp email with custom error message

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -2,23 +2,30 @@
 const nodemailer = require('nodemailer');
 
 async function sendVerificationEmail(verificationCode, to, subject) {
-  let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'engr.ahsan.bilal@gmail.com',
-      pass: '12345678899',
-    },
-  });
+  try {
+    let transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: 'engr.ahsan.bilal@gmail.com',
+        pass: '12345678899',
+      },
+    });
 
-  let info = await transporter.sendMail({
-    from: 'engr.ahsan.bilal@gmail.com', // sender address
-    to, // list of receivers
-    subject, // Subject line
-    text: '', // plain text body
-    html: `Your verification Code is <b>${verificationCode}</b>`, // html body
-  });
-  console.log('Message sent: %s', info.messageId);
-  console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    let info = await transporter.sendMail({
+      from: 'engr.ahsan.bilal@gmail.com', // sender address
+      to, // list of receivers
+      subject, // Subject line
+      text: '', // plain text body
+      html: `Your verification Code is <b>${verificationCode}</b>`, // html body
+    });
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  } catch (e) {
+    console.log(
+      `Unable to send SMTP verification email at ${to}, Verification Code is `,
+      verificationCode
+    );
+  }
 }
 
 module.exports = {
